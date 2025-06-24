7c4ed28d838f3f720eb6ae010c80c35e562e2fc3
mr. best <55440794+thebest2019@users.noreply.github.com>
2025-06-24 22:12:59 +0700

    Add files via upload

---
+++ disabled-guesture-script.ps1
@@ -0,0 +1,16 @@
+﻿# Run as Administrator
+
+# Disable Edge Swipe Gestures (EdgeUI)
+New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EdgeUI" -Force | Out-Null
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EdgeUI" -Name "AllowEdgeSwipe" -Value 0 -PropertyType DWORD -Force | Out-Null
+
+# Disable Help Tips (e.g., touch hints, UI tips)
+New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EdgeUI" -Force | Out-Null
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EdgeUI" -Name "DisableHelpTips" -Value 1 -PropertyType DWORD -Force | Out-Null
+
+# Optional: Apply to Current User as well
+New-Item -Path "HKCU:\Software\Policies\Microsoft\Windows\EdgeUI" -Force | Out-Null
+New-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\EdgeUI" -Name "DisableHelpTips" -Value 1 -PropertyType DWORD -Force | Out-Null
+
+# Restart the computer to apply changes
+Restart-Computer -Force
